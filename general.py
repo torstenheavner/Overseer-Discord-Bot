f761d1e44e7ff569bd080ee2634840f2c01787fd
Toaster <ttoasterrr@gmail.com>
2020-07-07 23:10:19 -0600

    Commented everything & Added o.delete

--- general.py
+++ general.py
@@ -6,12 +6,17 @@ import os
 import requests
 
 
+
 class General(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+
+
 	def cog_unload(self):
 		eou.log(text="Offline", cog="General", color="magenta")
+
+
 
     # @commands.Cog.listener()
     # async def on_message(self, message):
@@ -26,56 +31,121 @@ class General(commands.Cog):
     #                 pass
     #             await message.channel.send(message.author.name, file=discord.File("images/%s" % image))
 
-	@commands.command(brief="Create a new DM")
+
+
+	@commands.command(brief="Create a DM")
 	async def create(self, ctx, channelName, createVoice=False, *, people):
-		people = people.split(" ") # creates array of DM member usernames
-		for i in range(len(people)):
-			username = people[i] # stores usernames for error reporting
-			people[i] = discord.utils.get(self.bot.get_guild(729856235813470221).members, name=people[i]) # convert usernames to user IDs
-			if not people[i]:
-				return await ctx.send(embed=eou.makeEmbed(title="Whoops!", description="%s isn't in the server!" % username))
+		# o.create [channelName] [createVoice=False] [people]
+
+		# Initial variable setup
+		people = people.split(" ")
 		server = self.bot.get_guild(729856235813470221)
-		tc = await server.create_text_channel(channelName) # create the requested channel
+
+		# Change the array of names to an array of discord.Member objects
+		for i in range(len(people)):
+			username = people[i]
+			people[i] = discord.utils.get(server.members, name=people[i])
+			if not people[i]:
+				return await ctx.send(embed=eou.makeEmbed(title="Whoops!", description=f"{username} isn't in the server!"))
+
+		# Check if a channel with the given name alredy exists
+		if discord.utils.get(server.text_channels, name=channelName):
+			return await ctx.send(embed=eou.makeEmbed(title="Whoops!", description=f"A DM with the name \"{channelName}\" already exists."))
+
+		# Create a new channel and prevent @everyone from seeing it
+		tc = await server.create_text_channel(channelName)
 		await tc.set_permissions(ctx.guild.default_role, read_messages=False, send_messages=False)
+
+		# If necesarry, create matching voice channel and prevent #everyone from seeing it
 		if createVoice:
-			vc = await server.create_voice_channel(channelName) # create voice channel with same name if asked to
+			vc = await server.create_voice_channel(channelName)
 			await vc.set_permissions(ctx.guild.default_role, view_channel=False, connect=False)
+
+		# Allow everyone in the [people] array to see the channel(s)
 		for person in people:
 			await tc.set_permissions(person, read_messages=True, send_messages=True)
 			if createVoice:
 				await vc.set_permissions(person, view_channel=True, connect=False)
-		# change permissions so @everyone cant access, but the individuals can (for both the VC and TC)
-		await ctx.send(embed=eou.makeEmbed(title="Success!", description="Channel%s successfully created." % ("s" if createVoice else ""))) # send message confirmation
+
+		# Send ouput and log to console
+		await ctx.send(embed=eou.makeEmbed(title="Success!", description="Channel%s successfully created." % ("s" if createVoice else "")))
 		eou.log(text="New DM created", cog="General", color="magenta", ctx=ctx)
 
-	@commands.command(brief="Leave a specific channel") # o.leave
+
+
+	@commands.command(brief="Leave a DM")
 	async def leave(self, ctx, channelName):
-		member = ctx.author
+		# o.leave [channelName]
+
+		# Attempt to get the text and voice channels by the given name
 		tc = discord.utils.get(self.bot.get_guild(729856235813470221).text_channels, name=channelName)
 		vc = discord.utils.get(self.bot.get_guild(729856235813470221).voice_channels, name=channelName)
+
+		# Throw an error if you cant find any text channel
 		if not tc:
 			return await ctx.send(embed=eou.makeEmbed(title="Whoops!", description="I couldn't find that channel."))
-		await tc.set_permissions(member, overwrite=None) # remove all text channel perms from user
-		if vc:
-			await vc.set_permissions(member, overwrite=None)
-		await ctx.send(embed=eou.makeEmbed(title="Success!", description="%s has left %s." % (member.name, channelName)))
-		eou.log(text="Left %s" % (channelName), cog="General", color="magenta", ctx=ctx) # log the change
 
-	# o.delete?
+		# Get rid of the authors permissions for the necesarry channels
+		await tc.set_permissions(ctx.author, overwrite=None)
+		if vc:
+			await vc.set_permissions(ctx.author, overwrite=None)
+
+		# Send output and log to console
+		await ctx.send(embed=eou.makeEmbed(title="Success!", description=f"{ctx.author.name} has left {channelName}."))
+		eou.log(text=f"Left {channelName}", cog="General", color="magenta", ctx=ctx)
+
+
+
 	# o.add?
 	# o.kick?
 
-	@commands.command(brief="Rename a DM")
-	async def rename(self, ctx, channelName, newChannelName):
+
+
+	@commands.command(brief="Delete a DM")
+	async def delete(self, ctx, channelName):
+		# o.delete [channelName]
+
+		# Attempt to get the text and voice channels by the given name
 		tc = discord.utils.get(self.bot.get_guild(729856235813470221).text_channels, name=channelName)
 		vc = discord.utils.get(self.bot.get_guild(729856235813470221).voice_channels, name=channelName)
+
+		# Throw an error if you cant find any text channel
 		if not tc:
 			return await ctx.send(embed=eou.makeEmbed(title="Whoops!", description="I couldn't find that channel."))
+
+		# Delete the necesarry channels
+		await tc.delete()
+		if vc:
+			await vc.delete()
+
+		# Send output and log to console
+		await ctx.send(embed=eou.makeEmbed(title="Success!", description="%s successfully deleted." % channelName))
+		eou.log(text="DM deleted", cog="General", color="magenta", ctx=ctx)
+
+
+
+	@commands.command(brief="Rename a DM")
+	async def rename(self, ctx, channelName, newChannelName):
+		# o.rename [channelName] [newChannelName]
+
+		# Attempt to get the text and voice channels by the given name
+		tc = discord.utils.get(self.bot.get_guild(729856235813470221).text_channels, name=channelName)
+		vc = discord.utils.get(self.bot.get_guild(729856235813470221).voice_channels, name=channelName)
+
+		# Throw an error if you cant find any text channel
+		if not tc:
+			return await ctx.send(embed=eou.makeEmbed(title="Whoops!", description="I couldn't find that channel."))
+
+		# Change the names of the necesarry channels
 		await tc.edit(name=newChannelName)
 		if vc:
 			await vc.edit(name=newChannelName)
+
+		# Send output and log to console
 		await ctx.send(embed=eou.makeEmbed(title="Success!", description="%s sucessfully renamed to %s" % (channelName, newChannelName)))
 		eou.log(text="DM renamed", cog="General", color="magenta", ctx=ctx)
+
+
 
 def setup(bot):
 	eou.log(text="Online", cog="General", color="magenta")
